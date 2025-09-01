275db73d69d0f8d8f8b0200ff97100a699fb498d
SteamOS User <emptysea2000@gmail.com>
2025-08-31 23:48:11 -0400

    Added ending sequence.  Enemy AI and shell hiding are the last things to do.

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,20 +1,23 @@
 extends Node2D
 
-@onready var levels = [1,2,3]
-@onready var curLevel = 0
-@onready var health = 3
+enum MUSIC {LEVEL1,LEVEL2,LEVEL3,TITLE,ENDING,GAMEOVER}
+enum SPLASH {LEVEL1,LEVEL2,LEVEL3,TITLE,END1, END2, END3, END4, END5, END6,GAMEOVER}
+enum LEVEL {TITLE,LEVEL1,LEVEL2,LEVEL3,END1, END2, END3, END4, END5, END6}
+
+const PAUSE_BETWEEN_LEVEL = 3
+const PAUSE_BETWEEN_ENDING = 5
+const TURTLE_HEALTH = 3
+
+@onready var curLevel : LEVEL = LEVEL.TITLE
+@onready var health = TURTLE_HEALTH
 var cur_level_instance = ""
 var cur_music = ""
-
-enum MUSIC {LEVEL1,LEVEL2,LEVEL3,TITLE,ENDING,GAMEOVER}
-enum SPLASH {LEVEL1,LEVEL2,LEVEL3,TITLE,ENDING,GAMEOVER}
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	curLevel = 2
-	changeSplashScreen(SPLASH.LEVEL3)
-	changeLevelMusic(MUSIC.LEVEL3)
+	changeSplashScreen(SPLASH.LEVEL1)
+	changeLevelMusic(MUSIC.LEVEL1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -23,6 +26,11 @@ func _process(delta: float) -> void:
 func changeSplashScreen(toSplash : SPLASH) -> void:
 	var splashScreen
 	var splashInstance
+	var timerPause = PAUSE_BETWEEN_LEVEL
+
+	if toSplash >= SPLASH.END1:
+		timerPause = PAUSE_BETWEEN_ENDING
+
 	match toSplash:
 		SPLASH.LEVEL1:
 			splashScreen = load("res://Scenes/level1_intro.tscn")
@@ -30,10 +38,18 @@ func changeSplashScreen(toSplash : SPLASH) -> void:
 			splashScreen = load("res://Scenes/level2_intro.tscn")
 		SPLASH.LEVEL3:
 			splashScreen = load("res://Scenes/Level3_intro.tscn")
-		SPLASH.ENDING:
-			splashScreen = load("res://Scenes/level1_intro.tscn")
-		_:
-			splashScreen = load("res://Scenes/level1_intro.tscn")
+		SPLASH.END1:
+			splashScreen = load("res://Scenes/end_1.tscn")
+		SPLASH.END2:
+			splashScreen = load("res://Scenes/end_2.tscn")
+		SPLASH.END3:
+			splashScreen = load("res://Scenes/end_3.tscn")
+		SPLASH.END4:
+			splashScreen = load("res://Scenes/end_4.tscn")
+		SPLASH.END5:
+			splashScreen = load("res://Scenes/end_5.tscn")
+		SPLASH.END6:
+			splashScreen = load("res://Scenes/end_6.tscn")
 
 	$Life.hide()
 	splashInstance = splashScreen.instantiate()
@@ -42,7 +58,7 @@ func changeSplashScreen(toSplash : SPLASH) -> void:
 		cur_level_instance.queue_free()
 	cur_level_instance = splashInstance
 	add_child(splashInstance)
-	$SplashTimer.start(3)
+	$SplashTimer.start(timerPause)
 
 func changeLevels() -> void:
 	print ("Changing to next level...")
@@ -51,17 +67,18 @@ func changeLevels() -> void:
 	var nextMusic = ""
 	var nextMusicInst = ""
 	var ToNextEP = ""
-	
+
 	match curLevel:
-		1:
+		LEVEL.LEVEL1:
 			call_deferred("changeSplashScreen",SPLASH.LEVEL2)
 			call_deferred("changeLevelMusic",MUSIC.LEVEL2)
-		2:
+		LEVEL.LEVEL2:
 			call_deferred("changeSplashScreen",SPLASH.LEVEL3)
 			call_deferred("changeLevelMusic",MUSIC.LEVEL3)
-		3:
-			call_deferred("changeSplashScreen", SPLASH.ENDING)
+		LEVEL.LEVEL3:
+			call_deferred("changeSplashScreen",SPLASH.END1)
 			call_deferred("changeLevelMusic",MUSIC.ENDING)
+			pass
 		_:
 			print ("Unknown level state.  Ending game.")
 			queue_free()
@@ -71,38 +88,41 @@ func changeToLevel(toNextLevel: int) -> void:
 	var nextLevel_inst = ""
 	var ToNextEP = ""
 	match toNextLevel:
-		1:
-			curLevel = 1
+		LEVEL.LEVEL1:
+			curLevel = LEVEL.LEVEL1
 			nextLevel = load("res://Scenes/Level1.tscn")
 			nextLevel_inst = nextLevel.instantiate()
 			var nextLevelPoint = nextLevel_inst.get_node("ToNext")
 			var player = nextLevel_inst.get_node("Turtle")
 			var killzone = nextLevel_inst.get_node("KillZone")
-			
+
 			nextLevelPoint.connect("next_level",changeLevels)
 			player.connect("took_damage", took_damage)
 			killzone.connect("kill_player", took_damage)
-		2:
-			curLevel = 2
+		LEVEL.LEVEL2:
+			curLevel = LEVEL.LEVEL2
 			nextLevel = load("res://Scenes/level2.tscn")
 			cur_level_instance.queue_free()
 			nextLevel_inst = nextLevel.instantiate()
 			ToNextEP = nextLevel_inst.get_node("ToNext")
 			ToNextEP.connect("next_level",changeLevels)
-			
+
 			var killzone = nextLevel_inst.get_node("KillZone")
 			killzone.connect("kill_player", took_damage)
-			
+
 			var player = nextLevel_inst.get_node("Turtle")
 			player.connect("took_damage", took_damage)
-		3:
-			curLevel = 3
+		LEVEL.LEVEL3:
+			curLevel = LEVEL.LEVEL3
 			nextLevel = load("res://Scenes/level3.tscn")
 			cur_level_instance.queue_free()
 			nextLevel_inst = nextLevel.instantiate()
 			var rabbit = nextLevel_inst.get_node("Rabbit")
 			rabbit.connect("winner", changeLevels)
-	
+		_:
+			#Probably ending.  Don't set up another level.
+			pass
+
 	if is_instance_valid(cur_level_instance):
 		print("Freeing " + cur_level_instance.name + "...")
 		cur_level_instance.queue_free()
@@ -118,29 +138,58 @@ func changeLevelMusic(toMusic: MUSIC) -> void:
 			newMusic = load("res://Scenes/Level2_music.tscn")
 		MUSIC.LEVEL3:
 			newMusic = load("res://Scenes/Level3_music.tscn")
-
+		MUSIC.ENDING:
+			newMusic = load("res://Scenes/ending_song.tscn")
 	var music_instance = newMusic.instantiate()
-	
+
 	if is_instance_valid(cur_music):
 		cur_music.queue_free()
-	
+
 	cur_music = music_instance
 	add_child(music_instance)
 
 func took_damage(damage: int) -> void:
 	print ("Damage to take = " + str(damage))
 	$Life.take_damage(damage)
-	
+
 func _on_splash_timer_timeout() -> void:
 	match curLevel:
-		1:
-			changeToLevel(2)
-		2:
-			changeToLevel(3)
+		LEVEL.LEVEL1:
+			changeToLevel(LEVEL.LEVEL2)
+		LEVEL.LEVEL2:
+			changeToLevel(LEVEL.LEVEL3)
+		LEVEL.LEVEL3:
+			#Time to end the game. Use curLevel to keep track of which one we're on.
+			curLevel = LEVEL.END1
+			#Call the next splash screen.
+			changeSplashScreen(SPLASH.END2)
+		LEVEL.END1:
+			curLevel = LEVEL.END2
+			#Call the next splash screen.
+			changeSplashScreen(SPLASH.END3)
+		LEVEL.END2:
+			curLevel = LEVEL.END3
+			#Call the next splash screen.
+			changeSplashScreen(SPLASH.END4)
+		LEVEL.END3:
+			curLevel = LEVEL.END4
+			#Call the next splash screen.
+			changeSplashScreen(SPLASH.END5)
+		LEVEL.END4:
+			curLevel = LEVEL.END5
+			#Call the next splash screen.
+			changeSplashScreen(SPLASH.END6)
+		LEVEL.END5:
+			curLevel = LEVEL.END6
+			#Nothing left to do.
+		LEVEL.END6:
+			#Figure out what to do after the last wait timer goes off.
+			#For now, nothing.
+			pass
 		_:
 			changeToLevel(1)
-				
-	$Life.show()
+	if curLevel < LEVEL.END1:
+		$Life.show()
 
 func _on_life_game_over() -> void:
 	#Load our game over scene, and quit the current one.
