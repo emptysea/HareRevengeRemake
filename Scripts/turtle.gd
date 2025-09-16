ac3e307aa7d1eef7a46cc72b36370d4c9ad8f327
SteamOS User <emptysea2000@gmail.com>
2025-09-16 17:08:53 -0400

    Turtle no longer hit when hiding.  Original has collisions, too.  Do we add?

--- Scripts/turtle.gd
+++ Scripts/turtle.gd
@@ -13,7 +13,7 @@ enum FACING {LEFT, RIGHT}
 
 signal took_damage(damage_amount)
 func _ready() -> void:
-	$AnimatedSprite2D.play("Walk")
+	pass
 
 func _physics_process(delta: float) -> void:
 	
@@ -26,46 +26,52 @@ func _physics_process(delta: float) -> void:
 	#Implement hiding in shell.
 	if Input.is_action_just_pressed("Hide") and is_on_floor():
 		$AnimatedSprite2D.play("Hide")
-		pass
+		velocity.x = 0
+		is_hiding = true
+	
+	if Input.is_action_just_released("Hide"):
+		is_hiding = false
 
+	if not is_hiding:
 	# Handle jump.
-	if Input.is_action_just_pressed("Jump") and is_on_floor():
-		$Jump.play()
-		velocity.y = JUMP_VELOCITY
-	elif Input.is_action_just_pressed("Shoot") and is_on_floor():
-		var projectile = water.instantiate()
-		projectile.spawnPos = global_position
-		if facingDir == FACING.RIGHT:
-			projectile.spawnDir = 0
-		else:
-			var yAdjust = Vector2(0,26)
-			projectile.spawnPos = global_position + yAdjust
-			projectile.spawnDir = deg_to_rad(180)
+		if Input.is_action_just_pressed("Jump") and is_on_floor():
+			$Jump.play()
+			velocity.y = JUMP_VELOCITY
+		elif Input.is_action_just_pressed("Shoot") and is_on_floor():
+			var projectile = water.instantiate()
+			projectile.spawnPos = global_position
+			if facingDir == FACING.RIGHT:
+				projectile.spawnDir = 0
+			else:
+				var yAdjust = Vector2(0,26)
+				projectile.spawnPos = global_position + yAdjust
+				projectile.spawnDir = deg_to_rad(180)
 		
-		call_deferred("add_child",projectile)
+			call_deferred("add_child",projectile)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction and not StillLoading:
-		velocity.x = direction * SPEED
-		if velocity.x > 0:
-			facingDir = FACING.RIGHT
-			$AnimatedSprite2D.flip_h = false
-		elif velocity.x < 0:
-			facingDir = FACING.LEFT
-			$AnimatedSprite2D.flip_h = true
-		$AnimatedSprite2D.play("Walk")
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		$AnimatedSprite2D.stop()
-		$AnimatedSprite2D.set_frame_and_progress(0,0)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("ui_left", "ui_right")
+		if direction and not StillLoading:
+			velocity.x = direction * SPEED
+			if velocity.x > 0:
+				facingDir = FACING.RIGHT
+				$AnimatedSprite2D.flip_h = false
+			elif velocity.x < 0:
+				facingDir = FACING.LEFT
+				$AnimatedSprite2D.flip_h = true
+			$AnimatedSprite2D.play("Walk")
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			$AnimatedSprite2D.play("Walk")
+			$AnimatedSprite2D.stop()
+			$AnimatedSprite2D.set_frame_and_progress(0,0)
 
 	move_and_slide()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.visible == true:
+	if body.visible == true and not is_hiding:
 		$Hurt.play()
 		print ("Just hit a " + str(body)) # Replace with function body.
 		emit_signal("took_damage",1)
